ef81c20a717cb63cd163196634d28d2d3158765f
farrukh90 <farrukhsadykov@gmail.com>
2019-06-15 13:17:59 -0500

    added more lines

--- bootstrap/docker/docker_deploy.sh
+++ bootstrap/docker/docker_deploy.sh
@@ -5,5 +5,8 @@ sudo yum-config-manager --enable docker-ce-nightly
 sudo yum-config-manager --enable docker-ce-test
 sudo yum-config-manager --disable docker-ce-nightly
 sudo yum install docker-ce docker-ce-cli containerd.io -y 
+sudo yum install epel-release -y
+sudo yum install docker* -y
+
 sudo systemctl start docker
 sudo systemctl enable docker
