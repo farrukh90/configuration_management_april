e41beb630075450c6faf85d05ad4b810116b40d6
farrukh90 <farrukhsadykov@gmail.com>
2019-06-15 13:22:26 -0500

    added sleep

--- bootstrap/docker/docker_deploy.sh
+++ bootstrap/docker/docker_deploy.sh
@@ -5,6 +5,7 @@ sudo yum-config-manager --enable docker-ce-nightly
 sudo yum-config-manager --enable docker-ce-test
 sudo yum-config-manager --disable docker-ce-nightly
 sudo yum install docker-ce docker-ce-cli containerd.io -y 
+sleep 15
 sudo yum install epel-release -y
 sudo yum install docker* -y
 
